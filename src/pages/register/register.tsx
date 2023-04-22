6c41624ac7fd48fad13faebf05243124802240ea
guillaume-paris <guillaume1.paris@epitech.eu>
2023-04-22 20:08:43 +0200

    [ADD] fields states and submit functions

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './register.css';
 
 
 function Register() {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const [onRegister, setOnRegister] = useState(false);
+
+  function validateForm() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expression régulière pour vérifier l'adresse email
+  
+    if (!username || !email || !password) { // Vérifier si les champs sont remplis
+      alert('Veuillez remplir tous les champs');
+      return false;
+    } else if (!emailRegex.test(email)) { // Vérifier si l'adresse email est valide
+      alert('Veuillez entrer une adresse email valide');
+      return false;
+    }
+  
+    return true;
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    // Logique de traitement du formulaire
+    if (validateForm()) {
+      // Logique de traitement du formulaire si la validation est réussie
+      console.log('Formulaire soumis');
+    }
+  }
+
   return (
     <div className="flex min-h-screen">
       <div className="left-side w-1/3 flex flex-col items-center justify-center">
         <label className='text-5xl font-extrabold'> S'enregistrer</label>
-        <form className="flex flex-col gap-8 w-4/6">
-          <input type="text" name="username" className='register-text-field' placeholder="Nom d'utilisateur"/>
-          <input type="email" name="email" className='register-text-field' placeholder="Adresse email"/>
-          <input type="password" name="password" className='register-text-field' placeholder="Mot de passe"/>
+        <form className="flex flex-col gap-8 w-4/6" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            className="register-text-field"
+            placeholder="Nom d'utilisateur"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            type="email"
+            name="email"
+            className="register-text-field"
+            placeholder="Adresse email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            className="register-text-field"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
           <label className='text-sm font-normal w-11/12 text-center'>En vous enregistrant, vous acceptez les <a className='font-semibold'>Conditions d'utilisations</a> et <a className='font-semibold'>notre Politique de confidentialité</a></label>
-          <button type="submit" className='register-button mt-10'>S'inscrire</button>
+          <button type="submit" className='register-button mt-10' disabled={onRegister}>S'inscrire</button>
           <label className='flex justify-center font-normal'>Vous avez déjà un compte ? <a className='ms-1 font-extrabold'>Connexion</a></label>
         </form>
       </div>
