774b7cc504231423702ba3025c05ad764cea2e7a
guillaume-paris <guillaume1.paris@epitech.eu>
2023-06-29 01:10:31 +0200

    [ADD] storybook with tailwind import

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,10 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+module.exports = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
+  ],
+  framework: "@storybook/nextjs",
 };
-export default config;
